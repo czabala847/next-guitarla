3b0a23a6a23fb062d1ab0318f1cd22cc4123a636
Carlos Zabala <czabala847@gmail.com>
2023-08-11 23:14:12 -0500

    finish footer

--- components/footer.js
+++ components/footer.js
@@ -1,5 +1,23 @@
+import Link from "next/link";
 import React from "react";
 
+import Styles from "../styles/footer.module.css";
+
 export const Footer = () => {
-  return <div>footer</div>;
+  return (
+    <footer className={Styles.footer}>
+      <div className={`contenedor ${Styles.contenido}`}>
+        <nav className={Styles.navigation}>
+          <Link href="/">Inicio</Link>
+          <Link href="/about">Acerca de</Link>
+          <Link href="/store">Tienda</Link>
+          <Link href="/blog">Blog</Link>
+        </nav>
+
+        <p className={Styles.copyright}>
+          Todos los derechos reservados {new Date().getFullYear()}
+        </p>
+      </div>
+    </footer>
+  );
 };
