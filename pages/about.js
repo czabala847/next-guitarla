00a7149c03c773f40d96445408bdd98746a3495f
Carlos Zabala <czabala847@gmail.com>
2023-08-11 23:22:05 -0500

    page about{

--- pages/about.js
+++ pages/about.js
@@ -1,6 +1,8 @@
-import Link from "next/link";
+import Image from "next/image";
 import React from "react";
 import { Layout } from "../components/layout";
+
+import Style from "../styles/about.module.css";
 
 const About = () => {
   return (
@@ -8,8 +10,32 @@ const About = () => {
       title="About"
       description="Sobre nosotros Guitar LA, tienda de musica"
     >
-      <h1>About</h1>
-      <Link href="/">Ir al home</Link>
+      <main className="contenedor">
+        <h2 className="heading">Nosotros</h2>
+
+        <div className={Style.content}>
+          <Image
+            alt="sobre nosotros"
+            src="/img/nosotros.jpg"
+            width={1000}
+            height={800}
+          />
+          <div>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere
+              assumenda dolores neque molestias ipsum sint velit harum vel,
+              nostrum possimus ducimus, mollitia veritatis placeat consectetur
+              accusantium quibusdam reiciendis esse eaque?
+            </p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Iste
+              sapiente numquam ab, delectus perspiciatis aliquam distinctio
+              autem odit odio obcaecati libero repellat quidem consectetur
+              eligendi laborum? Consequatur enim facere voluptates?
+            </p>
+          </div>
+        </div>
+      </main>
     </Layout>
   );
 };
