2393d61de385999146485fe7e35b904f5411a8d5
Carlos Zabala <czabala847@gmail.com>
2023-08-11 23:04:40 -0500

    finish header

--- components/header.js
+++ components/header.js
@@ -1,19 +1,45 @@
 import Image from "next/image";
 import React from "react";
-
 import Link from "next/link";
+import { useRouter } from "next/router";
+
+import Styles from "../styles/header.module.css";
 
 export const Header = () => {
-  return (
-    <header>
-      <div className="contenedor">
-        <Image src="img/logo.svg" width={300} height={40} alt="Logo" />
+  const router = useRouter();
 
-        <nav>
-          <Link href="/">Inicio</Link>
-          <Link href="/about">Acerca de</Link>
-          <Link href="/store">Tienda</Link>
-          <Link href="/blog">Blog</Link>
+  return (
+    <header className={Styles.header}>
+      <div className={`contenedor ${Styles.barra}`}>
+        <Link href="/">
+          <Image src="img/logo.svg" width={300} height={40} alt="Logo" />
+        </Link>
+
+        <nav className={Styles.navigation}>
+          <Link
+            className={router.pathname === "/" ? Styles.active : ""}
+            href="/"
+          >
+            Inicio
+          </Link>
+          <Link
+            className={router.pathname === "/about" ? Styles.active : ""}
+            href="/about"
+          >
+            Acerca de
+          </Link>
+          <Link
+            className={router.pathname === "/store" ? Styles.active : ""}
+            href="/store"
+          >
+            Tienda
+          </Link>
+          <Link
+            className={router.pathname === "/blog" ? Styles.active : ""}
+            href="/blog"
+          >
+            Blog
+          </Link>
         </nav>
       </div>
     </header>
