780f81528218550c5821f195862bf14f24c28c7a
Carlos Zabala <czabala847@gmail.com>
2023-08-11 21:54:54 -0500

    add font from google font

---
+++ pages/_document.js
@@ -0,0 +1,32 @@
+import { Html, Head, Main, NextScript } from "next/document";
+
+export default function Document() {
+  return (
+    <Html>
+      <Head>
+        <meta
+          name="description"
+          content="Guitar LA - Venta de guitarras y blog de música"
+        />
+        <link rel="preconnect" href="https://fonts.googleapis.com" />
+        <link
+          rel="preconnect"
+          href="https://fonts.gstatic.com"
+          crossOrigin={"true"}
+        />
+        <link
+          href="https://fonts.googleapis.com/css2?family=Outfit:wght@400;700;900&family=Poppins:wght@400;600;700&family=Roboto:ital@1&display=swap"
+          rel="stylesheet"
+        />
+        <link
+          rel="stylesheet"
+          href="https://necolas.github.io/normalize.css/8.0.1/normalize.css"
+        />
+      </Head>
+      <body>
+        <Main />
+        <NextScript />
+      </body>
+    </Html>
+  );
+}
